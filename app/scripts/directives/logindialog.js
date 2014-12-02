743308270f3f70ca7f4e1804725bc1bcc0d5620b
Danilo Shibata <danilojun9@gmail.com>
2014-12-02 17:03:34 +0100

    Login dialog added (with bugs)

---
+++ app/scripts/directives/logindialog.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name myLoginApp.directive:loginDialog
+ * @description
+ * # loginDialog
+ */
+angular.module('myLoginApp')
+  .directive('loginDialog', function ($route, AUTH_EVENTS) {
+	  return {
+	    restrict: 'A',
+	    template: '<div ng-if="visible" ng-include="\'views/login.html\'">',
+	    controller: 'LoginCtrl',
+	    link: function (scope) {
+	      var showDialog = function () {
+	        scope.visible = true;
+	      };
+
+	      var hideDialog = function () {
+	        scope.visible = false;
+	      };
+	  
+	      scope.visible = false;
+
+	      scope.$on(AUTH_EVENTS.notAuthenticated, showDialog);
+	      scope.$on(AUTH_EVENTS.sessionTimeout, showDialog);
+
+	      scope.$on(AUTH_EVENTS.notNecessary, hideDialog);
+
+	      scope.$on(AUTH_EVENTS.loginSuccess, function () {
+	      	hideDialog();
+	      	$route.reload();
+	      });
+	    }
+	  };
+  });
